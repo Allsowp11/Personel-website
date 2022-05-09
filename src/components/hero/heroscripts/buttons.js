9142ba4ce37b562e75d661f94cc797b686cf0cf1
Allsowp11 <lberk1234qwerta@gmail.com>
2022-05-09 05:11:04 +0300

    update0.3
    some mistakes

--- src/components/hero/heroscripts/buttons.js
+++ src/components/hero/heroscripts/buttons.js
@@ -1,11 +1,11 @@
 export const Buttons = (info) =>{
     return(
         <div className={`${info.timeout ? "visible" : "invisible"} flex flex-row space-x-4 px-10`}>
-            <button className="text-2xl text-[#c4c4c4] hover:bg-[#171716] bg-[#262624] px-5 py-3 rounded-2xl">
+            <button className="text-2xl text-[#c4c4c4] hover:bg-[#171716] bg-[#262624] px-5 py-3 rounded-2xl" onClick={() => window.open("/blogs","_self")}>
                 Blogs
             </button>
-            <button className="text-2xl border-[#787777] hover:border-[#9c9a9a] text-[#A7D129] border-2 px-3 py-3 rounded-2xl">
-                about me
+            <button className="text-2xl border-[#787777] hover:border-[#9c9a9a] text-[#A7D129] border-2 px-3 py-3 rounded-2xl " onClick={() => window.open("/#About","_self")}>
+                About me
             </button>
         </div>
 
